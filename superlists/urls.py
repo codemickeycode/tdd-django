9901b8de4420f0b00ee0d324bbfa95203d68bfb9
codemickeycode <codemickeycode@gmail.com>
2014-07-29 17:15:47 +0800

    Chapter 6 messed up tests...

--- superlists/urls.py
+++ superlists/urls.py
@@ -7,4 +7,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     #url(r'^admin/', include(admin.site.urls)),
+    url(r'^lists/the-only-list-in-the-world/$', 'lists.views.view_list',
+        name='view_list'
+    ),
 )
